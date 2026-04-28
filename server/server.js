9bc072dedd501caa1b06643d8cfa8e8bd931dc22
Apoorv Sharma <apoorv03sharma@gmail.com>
2026-04-28 20:38:49 +0530

    downloads

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const connectDB = require('./config/db');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 // Middleware
 app.use(cors());
